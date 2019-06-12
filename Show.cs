b07b71ecf8f9109ec0c17917abcdc4a67f523d44
harbs0626 <51717561+harbs0626@users.noreply.github.com>
2019-06-12 15:58:22 -0400

    Add files via upload

---
+++ Show.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Assignment03_Theatre
+{
+    class Show
+    {
+        public double Price { get; }
+        public Day Day { get; }
+        public Movie Movie { get; }
+        public Time Time { get; }
+
+        public Show(Movie movie, Day day, double price, Time time)
+        {
+            Movie = movie;
+            Day = day;
+            Price = price;
+            Time = time;
+        }
+
+        public override string ToString()
+        {
+            return $"*** Movie ***\n{Movie}\nDay: {Day}\nPrice: {Price:C}\nTime: {Time}";
+        }
+
+    }
+}
